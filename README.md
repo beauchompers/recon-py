f2352de351ce901529b8980b0346e1557d8c817a
beauchompers <beauchompers@gmail.com>
2019-03-06 23:48:07 -0700

    updated recon.py to use python masscan module, and scan a cidr block

--- README.md
+++ README.md
@@ -4,14 +4,17 @@ Work in progress, various Python scripts for recon or network testing.
 
 ## recon.py
 
-Runs a masscan scan on a host to get open ports, and then runs nmap against those ports to grab services and versions.  Prints results to console, or optionally will output nmap results to a text file.
+Runs a masscan scan on a host or network to get open ports, and then runs nmap against those ports to grab services and versions.  Prints results to console, or optionally will output nmap results to a text file.  
+
+Will scan a network cidr (i.e. 192.168.8.0/24), however it's coded at the moment to only scan private ip ranges. Obviously only to be used on a network you have permission to scan.
 
 ```console
 $ ./recon.py --ip 192.168.8.8 --ports 0-1024
-$ ./recon.py --ip 192.168.8.8 --ports 0-65535 --file # outputs to 192.168.1.10.nmap.txt
+$ ./recon.py --ip 192.168.8.0/24 --ports 0-1024 # scan a cidr block
+$ ./recon.py --ip 192.168.8.8 --ports 0-65535 --file # outputs to $(pwd)/results/192.168.1.10.nmap.txt
 
 $ ./recon.py -h
-Recon Scan, recons a host using masscan/nmap
+Recon Scan
 usage: recon.py [-h] [--ip IP] [--ports PORTS] [--rate {500,1000}] [--file]
                 [--verbose] [--version]
 
@@ -19,7 +22,8 @@ Recon Scan
 
 optional arguments:
   -h, --help            show this help message and exit
-  --ip IP, -i IP        ip address to scan
+  --ip IP, -i IP        ip address or cidr (i.e 192.168.8.0/24) to scan. Note:
+                        Will only scan private networks.
   --ports PORTS, -p PORTS
                         port range to scan, example 0-443 or 22,135,443
   --rate {500,1000}, -r {500,1000}
--- recon.py
+++ recon.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-# simple nmap script to recon a host
+# simple nmap script to recon a host or network.
+# accepts an ip or cidr, and a range of ports.  
+# script has been coded to only allow scanning private networks for which you have permission.
 
+# modules
 import nmap
 import masscan
-import subprocess
 import sys
-import re
 import argparse
 import ipaddress
+import os
 
 # utility functions
 
@@ -46,15 +48,25 @@ def printdict(d, tabs=False):
         else:
             print("{} : {}".format(k, v))
 
-# validate params
-def validate_params(ip, ports):
-    #validate if the ip is actually an ip before we search
+# validate ip address or cidr block
+def validate_ip(ip):
+    #validate if the ip or cidr is actually an ip before we search
     try:
-        ipaddress.ip_address(ip)
+        ipaddress.ip_network(ip)
     except ValueError:
-        print("Error: {} is not a valid ip address...".format(ip))
+        print("Error: {} is not a valid ip or cidr block...".format(ip))
         sys.exit(1)
 
+    # with great power... only scan on private networks
+    try:
+        ipaddress.IPv4Network(ip).is_private 
+    except ValueError:
+        print("Error: {} is not a private network range or ip, script doesn't do that".format(ip))
+        sys.exit(1)
+
+    return True
+
+def validate_ports(ports):
     # validate the ports
     r = range(0,65536)
     try: 
@@ -72,48 +84,53 @@ def validate_params(ip, ports):
 
     return True
 
-# core functions
+# create folder for results if required
+def createfolder():
+    path = os.getcwd() + "/results/"
+    try:  
+        if not os.path.exists(path):
+            os.mkdir(path)
+    except OSError:  
+        print("Creation of the directory {} failed".format(path))
 
+    return path
+
+# core functions
 # masscan for ports
 def runmasscan(ip, ports, rate):
     # run massscan
+    args = "--max-rate {}".format(rate)
+    
+    print("Starting Masscan... grab a coffee...")
     try:
-        cmd = ["sudo", "masscan", "-p " + ports, "--max-rate", rate, ip]
-        print("\nMasscan Command: "+' '.join(cmd))
-        sp = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        output = ""
-        while True:
-            out = sp.stdout.read(1).decode('utf-8')
-            if out == '' and sp.poll() != None:
-                break
-            if out != '':
-                output += out
-                sys.stdout.write(out)
-                sys.stdout.flush()
-
-        # getting discovered ports from the masscan output and sorting them
-        results = re.findall("port (\d*)", output)
-        if results:
-            ports = list({int(port) for port in results})
-            ports.sort()
-
+        mas = masscan.PortScanner()
+        mas.scan(ip, ports=ports, arguments=args, sudo=True)
+        result = mas.scan_result['scan']
     except Exception as e:
         print("Error during Masscan: {} ".format(e))
         sys.exit(1)
 
-    return ports
+    print("Masscan Results:")
+    for host in result:
+        portlist = list(result[host]['tcp'].keys())
+        portlist.sort()
+        portlist = ','.join(map(str, portlist))
+        print("Host: {}, Ports: {}".format(host, portlist))
+
+    return result
 
 def runnmap(ip, ports, report=False, verbose=False):
     # run nmap
     ports = ','.join([str(port) for port in ports])
 
     if report:
-        args = "-A -oN " + ip + ".nmap.txt"
+        folder = createfolder()
+        args = "-A -oN " + folder + ip + ".nmap.txt"
     else: 
         args = "-A"
 
-    print("\nStarting NMap Scan...")
-    print("\nScanning the following ports: {}\n".format(ports))
+    print("\nStarting NMap Scan...this may take a while...")
+    print("\nScanning {} on the following ports: {}\n".format(ip, ports))
 
     try:
         nm = nmap.PortScanner() # instantiate nmap.PortScanner object
@@ -145,9 +162,9 @@ def __main__():
     # recon scan script
 
     # grab args,
-    print("Recon Scan, recons a host using masscan/nmap")
+    print("Recon Scan")
     parser = argparse.ArgumentParser(description='Recon Scan')
-    parser.add_argument('--ip', '-i', dest='ip', help='ip address to scan')
+    parser.add_argument('--ip', '-i', dest='ip', help='ip address or cidr (i.e 192.168.8.0/24) to scan. Note: Will only scan private networks.')
     parser.add_argument('--ports', '-p', dest='ports', default='0-65535', help='port range to scan, example 0-443 or 22,135,443')
     parser.add_argument('--rate', '-r', dest='rate', default='500', choices=['500', '1000'], help='rate for masscan packets, default 500, max 1000')
     parser.add_argument('--file', dest='file', action='store_true', default=False, help='output nmap results to a text file')
@@ -159,11 +176,17 @@ def __main__():
     checktools()
 
     # validate params before kicking things off
-    valid = validate_params(args.ip, args.ports)
+    validate_ip(args.ip)
+    validate_ports(args.ports)
 
-    if valid:
-        openports = runmasscan(args.ip, args.ports, args.rate)
-        runnmap(args.ip, openports, args.file, args.verbose)
+    # run masscan
+    scan = runmasscan(args.ip, args.ports, args.rate)
+
+    # run nmap against masscan results
+    for host in scan:
+        portlist = list(scan[host]['tcp'].keys())
+        portlist.sort()
+        runnmap(host, portlist, args.file, args.verbose)
 
     print("\nRecon completed...")
     sys.exit(0)
